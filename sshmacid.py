b6114991d55a51de5d6e18237086b47034e35ebf
Ryan <rpere16@gmail.com>
2017-11-09 11:07:25 -0600

    Update sshmacid.py
    Added comments

--- sshmacid.py
+++ sshmacid.py
@@ -5,49 +5,60 @@ import getpass
 from netmiko import ConnectHandler
 from operator import itemgetter, attrgetter
 
-#INIT
+#INIT NETMIKO CONNECTHANDLER STRINGS
 platform = 'cisco_ios'
 #host = '10.147.247.232'
 #user = 'admin'
 pw = getpass.getpass()
 
-#device = ConnectHandler(device_type=platform, ip=host, username=user, password=pw)
-
+#SAVE ARGS TO VARS CAUSE WHY NOT
 devicelist = sys.argv[1]
 maclist = sys.argv[2]
 user = sys.argv[3]
 ofilename = sys.argv[4]
 
+#OPEN OUTPUT FILE
 o = open(ofilename,'w')
 
+#TELL CONSOLE USER WHICH MACS ARE BEING SEARCHED FOR
 print "Searching for MAC Patterns: \n"
 with open(maclist,'r') as g:
         for item in g:
                 print item+'\n'
 
-#ITERATE THROUGH LIST OF SWITCHES CAUSE YEAH
+#OPEN LIST OF SWITCHES, ITERATE THROUGH AND DO WORK
 with open(devicelist,'r') as f:
         for ip in f:
         #FOR EVERY SWITCH IN LIST, OPEN SSH
+                #INFORM CONSOLE USER WTF IS GOING ON
                 print "Scanning: " + ip
+                #INIT OUTPUT VARIABLE
                 output=""
+                
+                #INIT AND CONNECT NETMIKO DEVICE
                 device = ConnectHandler(device_type=platform, ip=ip, username=user, password=pw)
+                
                 #SET TERM LEN TO 0
                 device.send_command("term len 0")
+                
                 #READ HOSTNAME
                 hostname = device.find_prompt()[:-1]
-                #CHECK FOR MACS DEFINED IN MAC LIST
+                
+                #CHECK CURRENT SWITCH FOR MACS DEFINED IN MAC LIST
                 with open(maclist,'r') as g:
                         for item in g:
                                 output = output + '\n' + device.send_command("show mac add | i "+item)
+                
                 #GTFO AND CLOSE
                 device.disconnect()
+                
                 #START FORMATTING BY SPLITTING BLOB INTO LINES
                 output = output.split('\n')
-                #INIT AND DECLARE LISTS
+                
+                #INIT AND DECLARE OUTPUT LIST
                 raw2 = [""]
-                #FOR EVERY ITEM IN LIST, CHOP AND SCREW LINES WITH MACS
-                #DEPENDING ON FORMAT OF SWITCH
+                
+                #SEARCH FOR MACS IN CISCO WAY OF DOING THINGS
                 for item in output:
                         if ("DYNAMIC" in item):
                                 raw2.append(item[38:])
@@ -58,4 +69,5 @@ with open(devicelist,'r') as f:
                 o.write( '\n' + hostname + ': ')
                 for item in sortout:
                         o.write(item+'\n')
+#SAY YOU'RE DONE
 print "Scan complete"
